13cf138a2ed606387c54f7ec87daa47bc7d59917
Evan Berg <evan.m.berg@gmail.com>
2015-11-09 12:41:49 -0500

    took out all about page stuff

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
 $(document).ready(function(){
   $('.membership_nav').on('click', function(){
     $('#membership').removeClass('inactive');
-    $('#about').addClass('inactive');
     $('#mailing_list').addClass('inactive');
   });
-  $('.about_nav').on('click', function(){
-    $('#about').removeClass('inactive');
-    $('#mailing_list').addClass('inactive');
-    $('#membership').addClass('inactive');
-  });
+
   $('.mailing_nav').on('click', function(){
     $('#mailing_list').removeClass('inactive');
-    $('#about').addClass('inactive');
     $('#membership').addClass('inactive');
   });
 
